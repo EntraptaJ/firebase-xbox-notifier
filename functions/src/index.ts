078266cf30d886012f037ca172c0557f5ff90d1e
Kristian Jones <kristianfjonesis@gmail.com>
2019-04-22 21:17:12 -0500

    feat: Stuff

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,91 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import {
+    PresenceResponse,
+    Game,
+    User as UserRecord,
+    XBoxDBRecord
+} from './types';
+
 admin.initializeApp(functions.config().firebase);
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+let Users: import('./types').User[] = []; // The array used for storing User data within this execution
+const XUIDs: string[] = [];
+const Games: string[] = [];
+
+const ProcessDocuments = async (doc: FirebaseFirestore.DocumentReference) => {
+    let document = await doc.get();
+    let data = document.data() as XBoxDBRecord;
+    data.Games.map(({ name }) => {
+        Games.push(name);
+    });
+    Users.push({
+        XUID: data.XUID,
+        username: doc.path.replace(/XBox\//, ''),
+        Games: data.Games,
+        notified: data.notified
+    });
+    XUIDs.push(data.XUID);
+    return;
+};
+
+const ProcessUserPresence = async (item: PresenceResponse): Promise<void> => {
+    let user = Users.find(a => a.XUID === item.xuid) as UserRecord;
+    if (!item.devices) {
+        await SetNotified(user.username, false);
+        return;
+    }
+    if (item.devices)
+        item.devices.map(({ titles }) =>
+            titles.map(({ name }) => {
+                if (!Games.includes(name)) return;
+                const { Subscribers } = user.Games.find(
+                    item => item.name == name
+                ) as Game;
+                Subscribers.map(async token => {
+                    const db = admin.firestore().collection('XBox');
+                    const { notified } = (await db
+                        .doc(user.username)
+                        .get()).data() as XBoxDBRecord;
+                    if (!notified)
+                        await SendNotification({
+                            token,
+                            name,
+                            username: user.username
+                        });
+                    return;
+                });
+            })
+        );
+};
+
+type SendNotificationArgs = {
+    token: string;
+    username: string;
+    name: string;
+};
+
+const SendNotification = async ({
+    token,
+    username,
+    name
+}: SendNotificationArgs) => {
+    admin.messaging().send({
+        token: token,
+        android: {
+            notification: {
+                tag: `MC-${username}-${name}`,
+                channelId: 'games-notify'
+            }
+        },
+        notification: {
+            title: 'XBox Notifier',
+            body: `${username} is Online on ${name}`
+        }
+    });
+
+    await SetNotified(username, true);
+};
 
 export const scheduledFunction = functions.pubsub
     .schedule(`every 1 minutes`)
@@ -16,101 +95,146 @@ export const scheduledFunction = functions.pubsub
             import('@xboxreplay/xboxlive-auth')
         ]);
 
-        const db = admin.firestore().collection('XBox');
-        const [{ userHash, XSTSToken }, docs] = await Promise.all([authenticate(
-            functions.config().xbox.usr,
-            functions.config().xbox.psw
-        ), db.listDocuments()])
-        const usrs: import('./types').User[] = [];
-        const Games: string[] = [];
-        const XUIDs: string[] = []
+        let db = admin.firestore().collection('XBox'); // The Firebase Firestore for storing Users subscription data.
+        let doc = db.listDocuments();
+        let [{ userHash, XSTSToken }, docs] = await Promise.all([
+            authenticate(
+                functions.config().xbox.usr,
+                functions.config().xbox.psw
+            ),
+            doc
+        ]);
 
-        const processDoc = async (item: FirebaseFirestore.DocumentReference) => {
-            const document = (await item.get()).data() as import('./types').XBoxDBRecord
-            document.Games.map(({ name }) => {
-                Games.push(name);
-            })
-            usrs.push({ XUID: document.XUID, username: item.path.replace(/XBox\//, ''), Games: document.Games })
-            XUIDs.push(document.XUID);
-            return;
-        }
-
-
-
-        await Promise.all(docs.map(item => processDoc(item)));
-        const { body }: { body: import('./types').PresenceResponse[] } = await got.post(
+        await Promise.all(docs.map(item => ProcessDocuments(item)));
+        const {
+            body
+        }: { body: import('./types').PresenceResponse[] } = await got.post(
             `https://userpresence.xboxlive.com/users/batch`,
             {
                 json: true,
                 headers: {
-                    'Accept': 'application/json',
+                    Accept: 'application/json',
                     'Content-Type': 'application/json',
                     'x-xbl-contract-version': 3,
-                    'Authorization': `XBL3.0 x=${userHash};${XSTSToken}`,
-                    'Accept-Language': 'en-US',
+                    Authorization: `XBL3.0 x=${userHash};${XSTSToken}`,
+                    'Accept-Language': 'en-US'
                 },
                 body: {
-                    onlineOnly: true,
                     users: XUIDs
                 }
             }
         );
+
+        await Promise.all(body.map(item => ProcessUserPresence(item)));
+        return;
+        /*
         body.map(item => {
-            const c = usrs.find(a => a.XUID == item.xuid) as import('./types').User
-            item.devices.map(({ titles }) => {
-                titles.map(({ name }) => {
-                    if (Games.includes(name)) {
-                        let { Subscribers } = c.Games.find((item) => item.name == name) as import('./types').Game;
-                        Subscribers.map((i) => {
-                            console.log(`Notifying ${i} about ${name}`)
-                            const MSG = admin.messaging()
-                            MSG.send({
-                                token: i,
-                                android: {
-                                    notification: {
-                                        tag: `MC-${c.username}`,
-                                        channelId: 'test-channel'
-                                    }
-                                },
-                                notification: {
-                                    title: 'XBox Notifier',
-                                    body: `${c.username} is Online on ${name}`
+            const c = usrs.find(
+                a => a.XUID == item.xuid
+            ) as import('./types').User;
+
+            if (item.devices) {
+                item.devices.map(({ titles }) => {
+                    titles.map(({ name }) => {
+                        if (Games.includes(name)) {
+                            let { Subscribers } = c.Games.find(
+                                item => item.name == name
+                            ) as import('./types').Game;
+                            Subscribers.map(i => {
+                                const MSG = admin.messaging();
+
+                                if (!c.notified) {
+                                    MSG.send({
+                                        token: i,
+                                        android: {
+                                            notification: {
+                                                tag: `MC-${c.username}-${name}`,
+                                                channelId: 'games-notify'
+                                            }
+                                        },
+                                        notification: {
+                                            title: 'XBox Notifier',
+                                            body: `${
+                                                c.username
+                                            } is Online on ${name}`
+                                        }
+                                    });
+                                    SetNotified(c.username, true);
                                 }
-                            })
-                        })
-                    }
-                })
-            })
-        })
-        return true;
+                            });
+                        }
+                    });
+                });
+            } else {
+                SetNotified(c.username, false);
+            }
+        }); */
     });
 
-export const AddSubscriber = functions.https
-    .onRequest(async (req, res) => {
-        const { username, token, game } = req.body.data as { username: string, token: string, game: string }
-        const db = admin.firestore().collection('XBox');
-        const doc = db.doc(username);
-        const document = await doc.get()
-        const exists = document.exists;
-        if (!exists) {
-            const [got] = await Promise.all([
-                import('got'),
-            ]);
-            const {
-                body: { xuid }
-            } = await got.get(`https://xboxapi.com/v2/xuid/${username}`, {
-                json: true,
-                headers: {
-                    'X-AUTH': functions.config().xbox.api
-                }
-            });
+const SetNotified = async (username: string, notified: boolean) => {
+    const db = admin.firestore().collection('XBox');
+    const doc = db.doc(username);
+    await doc.set(
+        {
+            notified: notified
+        },
+        { merge: true }
+    );
+    return;
+};
 
-            doc.create({ XUID: xuid, Games: [{ name: game, Subscribers: [token] }] })
-        } else {
-            const data = document.data() as import('./types').XBoxDBRecord;
-            if (!data.Games.some((item) => item.name == game)) await doc.set({ Games: [...data.Games, { name: game }] }, { merge: true });
-            const Game = data.Games.find(i => i.name == game) as import('./types').Game
-            if (!Game.Subscribers.some((item) => item == token)) await doc.set({ Games: [...data.Games, { name: game, Subscribers: [...Game.Subscribers, token] }] }, { merge: true });
+export const AddSubscriber = functions.https.onRequest(async (req, res) => {
+    const { username, token, game } = req.body.data as {
+        username: string;
+        token: string;
+        game: string;
+    };
+    const db = admin.firestore().collection('XBox');
+    const doc = db.doc(username);
+    const document = await doc.get();
+    const exists = document.exists;
+    if (!exists) {
+        const [got] = await Promise.all([import('got')]);
+        const {
+            body: { xuid }
+        } = await got.get(`https://xboxapi.com/v2/xuid/${username}`, {
+            json: true,
+            headers: {
+                'X-AUTH': functions.config().xbox.api
+            }
+        });
+
+        await doc.create({
+            XUID: xuid,
+            Games: [{ name: game, Subscribers: [token] }],
+            notified: false
+        });
+    } else {
+        const data = document.data() as import('./types').XBoxDBRecord;
+        if (!data.Games.some(item => item.name == game))
+            await doc.set(
+                {
+                    Games: [
+                        ...data.Games,
+                        { name: game, notified: false, Subscribers: [token] }
+                    ]
+                },
+                { merge: true }
+            );
+
+        const Game = data.Games.find(
+            i => i.name == game
+        ) as import('./types').Game;
+
+        if (Game && !Game.Subscribers.some(item => item == token)) {
+            data.Games[data.Games.indexOf(Game)].Subscribers.push(token);
+            await doc.set(
+                {
+                    Games: [...data.Games]
+                },
+                { merge: true }
+            );
         }
-        res.status(200).json({ data: { success: true } })
-    })
+    }
+    res.status(200).json({ data: { success: true } });
+});
--- functions/src/types.ts
+++ functions/src/types.ts
@@ -1,29 +1,31 @@
 export type XBoxDBRecord = {
     XUID: string;
     Games: Game[];
-}
+    notified: boolean;
+};
 
 export type PresenceResponse = {
-    xuid: string,
+    xuid: string;
     devices: {
-        type: string,
+        type: string;
         titles: {
-            id: string,
-            name: string,
-            lastModified: string,
-            state: string,
-            placement: string
-        }[]
-    }[]
-}
+            id: string;
+            name: string;
+            lastModified: string;
+            state: string;
+            placement: string;
+        }[];
+    }[];
+};
 
 export type Game = {
     name: string;
-    Subscribers: string[]
-}
+    Subscribers: string[];
+};
 
 export type User = {
-    XUID: string,
-    username: string,
-    Games: Game[]
-}
+    XUID: string;
+    username: string;
+    Games: Game[];
+    notified: boolean;
+};
